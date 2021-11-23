c36ff004317a396ba5940afaa309286b8c463179
SEA-W6-RR <90201728+SEA-W6-RR@users.noreply.github.com>
2021-11-23 15:50:06 +0100

    solution_quest_spring_REST

---
+++ src/main/java/com/springRestChallenge/library/repository/BookRepository.java
@@ -0,0 +1,15 @@
+package com.springRestChallenge.library.repository;
+
+import com.springRestChallenge.library.entities.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Integer> {
+
+    // custom query to search to blog post by title or content
+    List<Book> findByTitleContainingOrDescriptionContaining(String text, String textAgain);
+
+}
